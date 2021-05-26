022b6bf6b36b3abd9295152de287482fae9801eb
Shantanu Moghe <mogheshantanu@gmail.com>
2021-05-26 08:46:31 +0530

    CiCt - 1 - 1

---
+++ Chapter-1/unique.js
@@ -0,0 +1,24 @@
+//uses o^2 approach
+function isUnique(givenString){
+    var i = 0;
+    var isUnique = false;
+    var prevComb;
+
+    for(i=0; i<givenString.length; i++)
+    {
+        var a;
+        for(a=i+1; a< givenString.length ; a++ ){
+            if(givenString[a]==givenString[i]){
+                isUnique = true;
+            }
+        }
+        
+    }
+     return isUnique;
+}
+
+//const isUniquestring = isUnique('abd'); flase
+//const isUniquestring = isUnique('abddd'); true
+//const isUniquestring = isUnique('aabd'); tests
+
+console.log(isUniquestring);
